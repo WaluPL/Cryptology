5d8d54d9b1953d8841710a66a3a3cf99c757d1ac
Michał Walkiewicz <52602306+WaluPL@users.noreply.github.com>
2021-03-21 17:18:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var keySize = 256;
+var ivSize = 128;
+var iterations = 100;
+
+var message = window.prompt("Wpisz tekst do zaszyfrowania: ");
+var password = window.prompt("Wpisz haslo do szyfrowania: ");
+
+
+function encrypt (msg, pass) {
+  var salt = CryptoJS.lib.WordArray.random(128/8);
+  
+  var key = CryptoJS.PBKDF2(pass, salt, {
+      keySize: keySize/32,
+      iterations: iterations
+    });
+
+  var iv = CryptoJS.lib.WordArray.random(128/8);
+  
+  var encrypted = CryptoJS.AES.encrypt(msg, key, { 
+    iv: iv, 
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC
+    
+  });
+  
+  var transitmessage = salt.toString()+ iv.toString() + encrypted.toString();
+  return transitmessage;
+}
+
+function decrypt (transitmessage, pass) {
+  var salt = CryptoJS.enc.Hex.parse(transitmessage.substr(0, 32));
+  var iv = CryptoJS.enc.Hex.parse(transitmessage.substr(32, 32))
+  var encrypted = transitmessage.substring(64);
+  
+  var key = CryptoJS.PBKDF2(pass, salt, {
+      keySize: keySize/32,
+      iterations: iterations
+    });
+
+  var decrypted = CryptoJS.AES.decrypt(encrypted, key, { 
+    iv: iv, 
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC
+    
+  })
+  return decrypted;
+}
+
+var encrypted = encrypt(message, password);
+var decrypted = decrypt(encrypted, password);
+
+$('#encrypted').text("Encrypted: "+encrypted);
+$('#decrypted').text("Decrypted: "+ decrypted.toString(CryptoJS.enc.Utf8) );
